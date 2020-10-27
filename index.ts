4eb86f2cae356f7535e415a7edb17373e0654270
venkateshnayak2000 <43521737+venkateshnayak2000@users.noreply.github.com>
2020-10-27 20:09:01 +0530

    Update index.ts
    Commit 2

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import * as pluralize from 'pluralize'
 * @Method: Returns the plural form of any noun.
 * @Param {string}
 * @Return {string}
+* with comment
 */
 export function getPlural (str: any) : string {
   return pluralize.plural(str)
