0c38f0b9987993d35e1afcc6be97e236deb7d726
venkatesh <venkat_nayak2000@yahoo.co.in>
2020-11-01 17:28:22 +0530

    Third commit from intellij

--- index.ts
+++ index.ts
@@ -1,10 +1,10 @@
 import * as pluralize from 'pluralize'
 /**
-* Second change from intellij
+* Third change from intellij
 * @Method: Returns the plural form of any noun.
 * @Param {string}
 * @Return {string}
 */
 export function getPlural (str: any) : string {
-  return pluralize.plural(str);
+  return pluralize.plural(str); //Added back comment
 }
