4581358b718dc2f4b0b96cf66fad360d321a7e38
Venkatesh Nayak <venkat_nayak2000@yahoo.co.in>
2020-12-02 19:36:53 +0530

    Updated index.ts with branch commit

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import * as pluralize from 'pluralize'
 /**
-* 15 commit from server
+* 15 commit from server server again
 * @Method: Returns the plural form of any noun.
 * @Param {string}
 * @Return {string}
